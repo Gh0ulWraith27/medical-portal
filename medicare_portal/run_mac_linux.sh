396059e549c4d98cb763d776cda36ab42cdb0f6a
Gh0ulWraith27 <joshuareignmariano27@gmail.com>
2026-09-10 01:13:56 +0800

    Delete medicare_portal directory

--- medicare_portal/run_mac_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Create a virtual environment and install dependencies.
-python3 -m venv .venv
-source .venv/bin/activate
-python3 -m pip install -r requirements.txt
-python3 app.py
--- medicare_portal/run_windows.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Create a virtual environment and install dependencies.
-python -m venv .venv
-call .venv\Scripts\activate
-python -m pip install -r requirements.txt
-python app.py
